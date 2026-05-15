7716de880e3f24ac37e129f7b76fd9045f10863a
Geetanshup08 <geetanshup08@gmail.com>
2026-05-15 07:36:18 +0530

    Update API base URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://gaming-gear-java-backend.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
